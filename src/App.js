995baf4a49ebe905d219867bc42fbc734ee48527
Kevin Krato <kratok08@gmail.com>
2019-05-01 06:49:37 -0600

    Restructure app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="splash-container">
+        <h1>SwapiBox</h1>
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus
+          deserunt explicabo sunt repellat doloribus provident illo est corporis
+          voluptas dolorum? Impedit inventore accusamus, iusto corporis aperiam
+          quia veniam quae voluptatem.
+        </div>
+      </div>
+      <div className="card-container">
+        <p>Hello World</p>
+      </div>
     </div>
   );
 }
