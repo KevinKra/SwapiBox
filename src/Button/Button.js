91a515538f2eb5ca7d7333c2bfa67df794264ead
Kevin Krato <kratok08@gmail.com>
2019-05-02 16:57:04 -0600

    Migrate async functionality to button

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -14,30 +14,12 @@ class Button extends React.Component {
     };
   }
 
-  // componentDidMount() {
-  //   this.fetchAllData();
-  // }
-
-  // fetchData = () => {
-  //   fetch(`https://swapi.co/api/${this.props.label.toLowerCase()}`)
-  //     .then(answer => answer.json())
-  //     .then(unfilteredData => this.conditionalFilter(unfilteredData))
-  //     .then(filteredData => this.props.updateTopic(filteredData));
-  // };
-
-  /////////////
-  // fetchAllData = () => {
-  //   const peoplePromise = fetch(`https://swapi.co/api/people`);
-  //   const planetsPromise = fetch(`https://swapi.co/api/planets`);
-  //   const vehiclesPromise = fetch(`https://swapi.co/api/vehicles`);
-  //   Promise.all([peoplePromise, planetsPromise, vehiclesPromise])
-  //     // .then( responses.map( response => {response.json()} ).then( parsed => console.log(parsed));
-  //     .then(responses => console.log(responses));
-
-  //   // .then(answer => answer.json())
-  //   // .then(unfilteredData => this.conditionalFilter(unfilteredData))
-  //   // .then(filteredData => this.props.updateTopic(filteredData));
-  // };
+  fetchData = () => {
+    fetch(`https://swapi.co/api/${this.props.label.toLowerCase()}`)
+      .then(answer => answer.json())
+      .then(unfilteredData => this.conditionalFilter(unfilteredData))
+      .then(filteredData => this.props.updateTopic(filteredData));
+  };
 
   conditionalFilter = data => {
     if (this.props.label.toLowerCase() === "people") {
@@ -51,31 +33,7 @@ class Button extends React.Component {
     }
   };
 
-  filterCombiner = data => {
-    // const { planetData, personData, vehicleData }
-    const combinedData = data.reduce((accum, curr) => {
-      const planetData = filterDataPlanet();
-      const personData = filterDataPerson();
-      const vehicleData = filterDataVehicle();
-      accum.push(planetData, personData, vehicleData);
-      return accum;
-    }, []);
-    return combinedData;
-  };
-
-  // filterCombiner = data => {
-  //   const combinedData = data.reduce((accum, curr) => {
-  //     const planetData = filterDataPlanet();
-  //     const personData = filterDataPerson();
-  //     const vehicleData = filterDataVehicle();
-  //     accum.push(planetData, personData, vehicleData);
-  //     return accum;
-  //   }, []);
-  //   return combinedData;
-  // };
-
   render() {
-    console.log(this.props.label.toLowerCase());
     return (
       <button className="btn-1" onClick={this.fetchData}>
         {this.props.label}
