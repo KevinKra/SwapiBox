604ff4c0f4313d9777462bd8cfb463dfb406120f
Kevin Krato <kratok08@gmail.com>
2019-05-02 06:14:57 -0600

    Migrate state and fetch in SplashContainer

--- src/App.js
+++ src/App.js
@@ -2,26 +2,8 @@ import React from "react";
 import "./_App.scss";
 import SplashContainer from "./SplashContainer/SplashContainer";
 import ExploreContainer from "./ExploreContainer/ExploreContainer";
-import { clearRandomMovie } from "./utilities";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cards: [],
-      randomMovie: {}
-    };
-  }
-
-  componentDidMount() {
-    const randomNumber = Math.floor(Math.random() * 7) + 1;
-    fetch(`https://swapi.co/api/films/${randomNumber}`)
-      .then(response => response.json())
-      .then(result => clearRandomMovie(result))
-      .then(result => this.setState({ randomMovie: { ...result } }))
-      .catch(() => console.log("unsuccessful film crawl fetch"));
-  }
-
   render() {
     return (
       <div className="App">
--- src/SplashContainer/SplashContainer.js
+++ src/SplashContainer/SplashContainer.js
@@ -1,28 +1,43 @@
 import React from "react";
 import "./_SplashContainer.scss";
+import { clearRandomMovie } from "../utilities";
+import Crawl from "react-star-wars-crawl";
+import "react-star-wars-crawl/lib/index.css";
 
 class SplashContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      randomMovie: {}
+    };
+  }
+
+  componentDidMount() {
+    const randomNumber = Math.floor(Math.random() * 7) + 1;
+    fetch(`https://swapi.co/api/films/${randomNumber}`)
+      .then(response => response.json())
+      .then(result => clearRandomMovie(result))
+      .then(result => this.setState({ randomMovie: { ...result } }))
+      .catch(() => console.log("unsuccessful film crawl fetch"));
+  }
+
   render() {
+    let { summary, title, releaseDate } = this.state.randomMovie;
+
     return (
       <section className="splash-container">
         <div className="title-content">
           <h1>Swapi-Box</h1>
           <p>Explore the StarWars Universe</p>
         </div>
-        <p className="film-prologue">
-          It looks like Sandpeople did this, all right. Look, here are Gaffi
-          sticks, Bantha tracks. It’s just…I never heard of them hitting
-          anything this big before. They didn’t. But we are meant to think they
-          did. These tracks are side by side. Sandpeople always ride single file
-          to hide there numbers. These are the same Jawas that sold us Artoo and
-          Threepio. And these blast points, too accurate for Sandpeople. Only
-          Imperial stormtroopers are so precise. Why would Imperial troops want
-          to slaughter Jawas? If they traced the robots here, they may have
-          learned who they sold them to. And that would lead them home! Wait,
-          Luke! It’s too dangerous. Uncle Owen! Aunt Beru! Uncle Owen!
-          <span className="movie-title">A New Hope</span>
-          <span className="movie-year">1977 - 05 - 25</span>
-        </p>
+        <Crawl>
+          <div className="film-prologue">
+            <h1 className="movie-title">{title}</h1>
+            <p>{summary}</p>
+            <p className="movie-year">{releaseDate}</p>
+          </div>
+        </Crawl>
       </section>
     );
   }
