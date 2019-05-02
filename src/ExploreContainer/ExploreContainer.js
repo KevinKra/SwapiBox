15c7fe2bdf8a940cafa02d4e753d70c7f5bf6d39
Kevin Krato <kratok08@gmail.com>
2019-05-02 13:41:55 -0600

    Migrate in fetching  all data

--- src/ExploreContainer/ExploreContainer.js
+++ src/ExploreContainer/ExploreContainer.js
@@ -1,15 +1,44 @@
 import React from "react";
 import "./_ExploreContainer.scss";
+import Button from "../Button/Button";
 
 class ExploreContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTopic: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAllData();
+  }
+
+  updateTopic = newTopic => {
+    this.setState({ currentTopic: newTopic });
+  };
+
+  fetchAllData = () => {
+    const urls = [
+      `https://swapi.co/api/people`,
+      `https://swapi.co/api/planets`,
+      `https://swapi.co/api/vehicles`,
+      `https://swapi.co/api/species`
+    ];
+    const resolvedPromises = urls.map(url =>
+      fetch(url).then(response => response.json())
+    );
+    Promise.all(resolvedPromises).then(response => console.log(response));
+  };
+
   render() {
     return (
       <section className="card-container">
         <h1>EXPLORE</h1>
         <div className="btn-section">
-          <button className="btn-1">PEOPLE</button>
-          <button className="btn-1">PLANETS</button>
-          <button className="btn-1">VEHICLES</button>
+          <Button label={"PEOPLE"} />
+          <Button label={"PLANETS"} />
+          <Button label={"VEHICLES"} />
         </div>
         <a className="view-favorites" href="">
           View Favorites
