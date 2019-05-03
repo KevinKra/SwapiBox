a078c4e5b94ce086264fd7dc028f8bc4cc402ff4
Kevin Krato <kratok08@gmail.com>
2019-05-02 19:06:52 -0600

    Retrieve full person data

--- src/utilities.js
+++ src/utilities.js
@@ -11,16 +11,46 @@ export const filterDataPerson = data => {
   const output = data.results.reduce((accum, person) => {
     const result = {
       name: person.name,
-      homeworld: "fetch needed 2",
-      homeworld_pop: "fetch needed 1",
-      species: "fetch needed 3"
+      homeworld: person.homeworld,
+      species: person.species
     };
     accum.push(result);
     return accum;
   }, []);
-  return output;
+  return fetchData(output);
 };
 
+const fetchData = peopleArray => {
+  const promises = peopleArray.map(person => {
+    return fetch(person.homeworld)
+      .then(response => response.json())
+      .then(data => ({
+        ...person,
+        homeworld: data.name,
+        population: data.population
+      }));
+  });
+  Promise.all(promises)
+    .then(result => fetchSpecies(result))
+    .then(output => console.log("output", output));
+};
+
+const fetchSpecies = peopleArray => {
+  console.log("input", peopleArray);
+  console.log("species URL", peopleArray[0].species[0]);
+  const promises = peopleArray.map(person =>
+    fetch(person.species[0])
+      .then(response => response.json())
+      // .then(shape => console.log(shape))
+      .then(data => ({
+        ...person,
+        species: data.name
+      }))
+  );
+  return Promise.all(promises);
+};
+
+///////
 export const filterDataPlanet = data => {
   const output = data.results.reduce((accum, planet) => {
     const result = {
