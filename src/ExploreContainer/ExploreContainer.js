78a937d2a433b0db7bbea5ed6aea2bc5c86b73fe
Kevin Krato <kratok08@gmail.com>
2019-05-05 19:32:39 -0600

    Build Card container

--- src/ExploreContainer/ExploreContainer.js
+++ src/ExploreContainer/ExploreContainer.js
@@ -1,47 +1,57 @@
 import React from "react";
 import "./_ExploreContainer.scss";
 import Button from "../Button/Button";
+import CardContainer from "../CardContainer/CardContainer";
 
 class ExploreContainer extends React.Component {
   constructor() {
     super();
     this.state = {
       people: {},
-      planets: {}
+      planets: {},
+      vehicles: {},
+      currentTopic: ""
     };
   }
-
-  updatePeople = people => {
-    console.log(people);
-    console.log("UpdateTopic has been fired");
-    this.setState({ people });
-  };
 
   updateState = (targetState, newState) => {
     console.log(targetState);
     console.log("UpdateState has been fired");
-    this.setState({ [targetState]: newState });
+    this.setState({ [targetState]: newState, currentTopic: targetState });
   };
 
   render() {
     console.log(this.state);
+    const { people, planets, vehicles, currentTopic } = this.state;
     return (
       <section className="card-container">
         <h1>EXPLORE</h1>
         <div className="btn-section">
-          <Button updateState={this.updateState} label={"PEOPLE"} />
-          <Button updateState={this.updateState} label={"PLANETS"} />
-          <Button updateState={this.updateState} label={"VEHICLES"} />
+          <Button
+            className="btn-1"
+            updateState={this.updateState}
+            label={"PEOPLE"}
+          />
+          <Button
+            className="btn-1"
+            updateState={this.updateState}
+            label={"PLANETS"}
+          />
+          <Button
+            className="btn-1"
+            updateState={this.updateState}
+            label={"VEHICLES"}
+          />
         </div>
         <a className="view-favorites" href="">
           View Favorites
         </a>
-        <div className="card-section">
-          <article className="output-card">CARD</article>
-          <article className="output-card">CARD</article>
-          <article className="output-card">CARD</article>
-          <article className="output-card">CARD</article>
-        </div>
+        <CardContainer
+          currentTopic={currentTopic}
+          vehicles={vehicles}
+          people={people}
+          planets={planets}
+        />
       </section>
     );
   }
